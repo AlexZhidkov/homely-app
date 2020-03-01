05b5a5fc75962dad69001455c6564f22355897c9
Alexander Zhidkov <azhidkov@gmail.com>
2020-03-01 12:27:29 +0800

    basic bricks selection

--- src/app/client/addenda-selection/addenda-selection.component.ts
+++ src/app/client/addenda-selection/addenda-selection.component.ts
@@ -13,16 +13,36 @@ export class AddendaSelectionComponent implements OnInit {
   regConfig: FieldConfig[] = [
     {
       type: 'card',
-      label: 'Username',
+      label: 'Redwood',
+      subtitle: '$330.33',
+      description: 'The double height bricks of the Manor Range, with their refined textures and subtle colours, are a classic choice for traditional and contemporary homes. ',
+      imageUrl: 'https://www.midlandbrick.com.au/getmedia/1903c1f4-53b8-4dc2-943f-56555f9f1691/bricks-manor-redwood-1000.jpg?maxsidesize=220',
       inputType: 'text',
-      name: 'name'
+      value: 'Midland Brick - Redwood'
+    },
+    {
+      type: 'card',
+      label: 'Amber Glow',
+      subtitle: '$330.33',
+      description: 'The double height bricks of the Manor Range, with their refined textures and subtle colours, are a classic choice for traditional and contemporary homes.',
+      imageUrl: 'https://www.midlandbrick.com.au/getmedia/ec917804-2c7e-4b3c-86e8-0f8839e15e8e/bricks-manor-amber-glow-1000.jpg?maxsidesize=220',
+      inputType: 'text',
+      value: 'Midland Brick - Amber Glow'
+    },
+    {
+      type: 'card',
+      label: 'Augusta Limestone',
+      subtitle: '$330.33',
+      description: 'The double height bricks of the Manor Range, with their refined textures and subtle colours, are a classic choice for traditional and contemporary homes.',
+      imageUrl: 'https://www.midlandbrick.com.au/getmedia/34006434-2bf0-4b48-89b2-2cb70de99ccc/bricks-manor-augusta-limestone-1000.jpg?maxsidesize=220',
+      inputType: 'text',
+      value: 'Midland Brick - Augusta Limestone'
     },
   ];
 
   constructor() { }
 
   ngOnInit(): void {
-
   }
 
   submit(event: any) {
--- src/app/model/fieldConfig.ts
+++ src/app/model/fieldConfig.ts
@@ -3,6 +3,9 @@ import { Validator } from './validator';
 export interface FieldConfig {
     label?: string;
     name?: string;
+    subtitle?: string;
+    imageUrl?: string;
+    description?: string;
     inputType?: string;
     options?: string[];
     collections?: any;
