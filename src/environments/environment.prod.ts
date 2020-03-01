d7acfdfc54ca2a4a8ea12a8c234714f763ebf4d8
Alexander Zhidkov <azhidkov@gmail.com>
2020-03-01 13:27:19 +0800

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDqRO8LXE0sAbQ5GYVDzr06kH1694opDi8',
+    authDomain: 'ighar-dev.firebaseapp.com',
+    databaseURL: 'https://ighar-dev.firebaseio.com',
+    projectId: 'ighar-dev',
+    storageBucket: 'ighar-dev.appspot.com',
+    messagingSenderId: '995527743634',
+    appId: '1:995527743634:web:27e133201d530b6c7a6e4b'
+  }
 };
