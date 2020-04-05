b8920680a5f894a9af2170d60f60fddeed30437f
Alexander Zhidkov <azhidkov@gmail.com>
2020-04-05 14:11:32 +0800

    reset addenda store

--- src/app/client/dynamic-selection-form/dynamic-selection-form.component.ts
+++ src/app/client/dynamic-selection-form/dynamic-selection-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { DynamicFormComponent } from 'src/app/dynamic-components/dynamic-form/dynamic-form.component';
+import { AddendaStoreService } from 'src/app/services/addenda-store.service';
 
 @Component({
   selector: 'app-dynamic-selection-form',
@@ -11,7 +12,10 @@ export class DynamicSelectionFormComponent implements OnInit {
   @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
   dynamicFormDoc: AngularFirestoreDocument<any>;
   dynamicForm: any;
-  constructor(private afs: AngularFirestore) { }
+  constructor(
+    private afs: AngularFirestore,
+    public addendaStore: AddendaStoreService
+  ) { }
 
   ngOnInit(): void {
     this.dynamicFormDoc = this.afs.doc<any>('dynamic-form/external');
