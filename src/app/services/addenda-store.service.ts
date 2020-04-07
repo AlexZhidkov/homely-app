d6fcd865599f55093ce75a18d98cf7c5e00555fc
Alexander Zhidkov <azhidkov@gmail.com>
2020-04-07 19:54:09 +0800

    add all steps to sections

--- src/app/services/addenda-store.service.ts
+++ src/app/services/addenda-store.service.ts
@@ -24,14 +24,111 @@ export class AddendaStoreService {
 
   getSections() {
     return [
-      { id: 'external', label: 'External', steps: ['Bricks', 'Colorbond', 'Roof Tiles', 'Guttering', 'Entry Doors', 'Entry Door Furniture', 'Garage Doors', 'Paving', 'Hot Water Systems', 'Solar Systems'] },
-      { id: 'internal', label: 'Internal', steps: ['', ] },
-      { id: 'kitchen', label: 'Kitchen', steps: ['', ] },
-      { id: 'scullery', label: 'Scullery', steps: ['', ] },
-      { id: 'laundry', label: 'Laundry', steps: ['', ] },
-      { id: 'ens', label: 'Ens And Bathrooms', steps: ['', ] },
-      { id: 'wc', label: 'WC & PDR Rooms', steps: ['', ] },
-      { id: 'electrical', label: 'Electrical', steps: ['', ] },
+      {
+        id: 'external', label: 'External',
+        steps: [
+          'Bricks',
+          'Colorbond',
+          'Roof Tiles',
+          'Guttering',
+          'Entry Doors',
+          'Entry Door Furniture',
+          'Garage Doors',
+          'Paving',
+          'Hot Water Systems',
+          'Solar Systems'
+        ]
+      },
+      {
+        id: 'internal', label: 'Internal',
+        steps: [
+          'Internal Doors',
+          'Internal Door Furniture',
+          'Robe Doors',
+          'Cornice',
+          'Wet-area Tiling',
+          'Air Conditioning',
+          'Wall Painting',
+          'Balustrade'
+        ]
+      },
+      {
+        id: 'kitchen', label: 'Kitchen',
+        steps: [
+          'Sinks',
+          'Tapware',
+          'Ovens',
+          'Cooktops',
+          'Range Hoods',
+          'Dishwashers',
+          'Countertops',
+          'Overhead Cupboards',
+          'Cupboard And Cupboard Doors'
+        ]
+      },
+      {
+        id: 'scullery', label: 'Scullery',
+        steps: [
+          'Sinks',
+          'Tapware',
+          'Ovens',
+          'Cooktops',
+          'Range Hoods',
+          'Dishwashers',
+          'Countertops',
+          'Overhead Cupboards',
+          'Cupboard And Cupboard Doors'
+        ]
+      },
+      {
+        id: 'laundry', label: 'Laundry',
+        steps: [
+          'Troughs',
+          'Tapware',
+          'Dishwashers',
+          'Countertops',
+          'Overhead Cupboards',
+          'Cupboard And Cupboard Doors'
+        ]
+      },
+      {
+        id: 'ens', label: 'Ens And Bathrooms',
+        steps: [
+          'Cupboard And Cupboard Doors',
+          'Counter Tops',
+          'Mirrors',
+          'Basins',
+          'Tapware',
+          'Shower Screens',
+          'Shower Heads',
+          'Shower shop dishes',
+          'Towel Rails & Racks',
+          'Bath Tubs',
+          'Bath Tub Tapware'
+        ]
+      },
+      {
+        id: 'wc', label: 'WC & PDR Rooms',
+        steps: [
+          'Toilet Suites',
+          'Toilet Roll Holders',
+          'Cupboard And Cupboard Doors',
+          'Counter Tops',
+          'Mirrors',
+          'Basins',
+          'Tapware',
+          'Towel Rails And Racks'
+        ]
+      },
+      {
+        id: 'electrical', label: 'Electrical',
+        steps: [
+          'Power-Points And Switches',
+          'Lighting',
+          'Exhaust Fans',
+          'Fans',
+          'Smart Wiring']
+      },
     ];
   }
 }
